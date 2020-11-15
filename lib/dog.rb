f7fbf8156994381341828ecaa19099a1ef3133de
Erick Castille <ecastille924@gmail.com>
2020-11-15 22:14:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,8 +55,9 @@ class Dog
   end
   
   def self.find_by_id(id)
-    sql = "SELECT * FROM dogs WHERE id = ?"
-    DB[:conn].execute(sql, id).map do |row|
-    self.new_from_db(row)
-  end.first
+     sql = "SELECT * FROM dogs WHERE id = ?"
+     DB[:conn].execute(sql, id).map do |row|
+     self.new_from_db(row)
+    end.first
+  end
 end

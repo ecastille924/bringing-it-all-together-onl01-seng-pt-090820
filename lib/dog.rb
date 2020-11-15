c678aad5117b88d4cdc3f6a6e01eeadaba6a44a2
Erick Castille <ecastille924@gmail.com>
2020-11-15 22:24:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -80,6 +80,7 @@ class Dog
   
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
-    DB[:conn].execute(sql, name)
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db
   end
 end
